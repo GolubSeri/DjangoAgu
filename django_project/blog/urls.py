cb6612ed22131f78756370314191d76eba104722
GolubSeri <78564732+GolubSeri@users.noreply.github.com>
2023-03-31 15:20:49 +0300

    Add files via upload

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView, CustomForm
+from .views import PostListView, PostDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView, CustomForm, CommentDelete
 
 urlpatterns = [
     path("form/", CustomForm, name="custom_form"),
 
     path("post/new/", BlogCreateView.as_view(), name="post_new"),
-    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
     path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
     path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
-    path("", BlogListView.as_view(), name="home"),
+    path("post/<int:pk>/com_delete/", CommentDelete.as_view(), name="comment_delete"),
+    path("", PostListView.as_view(), name="home"),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,11 +1,12 @@
+from django.views import View
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Post
-from django.urls import reverse_lazy
+from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from .models import Post, Comment
+from django.urls import reverse_lazy, reverse
 from django.http import HttpResponse
-
 from django.shortcuts import render
-from .forms import UserForm
+from .forms import UserForm, CommentForm
 
 
 def CustomForm(request):
@@ -18,15 +19,56 @@ def CustomForm(request):
         return render(request, "custom_form.html", {"form": userform})
 
 
-class BlogListView(ListView):
+class PostListView(ListView):
     model = Post
     paginate_by = 2
     template_name = "home.html"
 
 
-class BlogDetailView(DetailView):
+class PostDisplay(DetailView):
     model = Post
-    template_name = "post_detail.html"
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        return context
+
+
+class PostComment(SingleObjectMixin, FormView):
+    model = Post
+    form_class = CommentForm
+    template_name = 'post_detail.html'
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super(PostComment, self).get_form_kwargs()
+        kwargs['request'] = self.request
+        return kwargs
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = self.object
+        comment.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        post = self.get_object()
+        return reverse('post_detail', kwargs={'pk': post.pk}) + '#comments'
+
+
+class PostDetailView(View):
+    def get(self, request, *args, **kwargs):
+        view = PostDisplay.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = PostComment.as_view()
+        return view(request, *args, **kwargs)
 
 
 class BlogCreateView(CreateView):
@@ -46,3 +88,8 @@ class BlogDeleteView(DeleteView):
     template_name = "post_delete.html"
     success_url = reverse_lazy("home")
 
+
+class CommentDelete(DeleteView):
+    model = Comment
+    template_name = "comment_delete.html"
+    success_url = reverse_lazy("home")
